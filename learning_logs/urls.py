f95d0ddb13cf356b894878ee11ccc28765c0f3e8
Hemant Singh <striker.hemant@gmail.com>
2019-03-20 10:30:51 -0700

    Added first

---
+++ learning_logs/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+app_name='app1'
+
+
+urlpatterns = [
+    #url(r'^$', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    #path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
